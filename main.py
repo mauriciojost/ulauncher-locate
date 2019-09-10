dde98e91a1857001ac4e1da147da7a436094add9
Mauricio Jost <mauricio.jost@gmail.com>
2019-09-10 07:39:52 +0200

    Better use RunScriptAction
    Cleanup of code using API of ulauncher.

--- main.py
+++ main.py
@@ -16,20 +16,14 @@ from ulauncher.api.shared.item.ExtensionSmallResultItem import ExtensionSmallRes
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
 from ulauncher.api.shared.action.OpenAction import OpenAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
 
 
 logger = logging.getLogger(__name__)
 ext_icon = 'images/icon.png'
 exec_icon = 'images/executable.png'
 dead_icon = 'images/dead.png'
-
-action_update = 'U'
-action_locate = 'L'
-
 database_filepath = os.path.join(os.getenv("HOME"), '.locate-ulauncher-database')
-
-
-
 
 class LocateExtension(Extension):
 
@@ -55,47 +49,31 @@ class KeywordQueryEventListener(EventListener):
         for pref_id, pref_value in list(extension.preferences.items()):
             if pref_value == keyword:
                 keyword_id = pref_id
-            elif pref_id == 'flags':
+            elif pref_id == 'locate_flags':
                 locate_flags = pref_value
+            elif pref_id == 'open_script':
+                open_script = pref_value
 
-        if keyword_id == "locate":
+        if keyword_id == "locate_keyword":
             # locate a file in database
-            return RenderResultListAction(list(islice(self.generate_results(extension, pattern, locate_flags), 15)))
-        elif keyword_id == "update":
+            return RenderResultListAction(list(islice(self.generate_results(extension, pattern, locate_flags, open_script), 100)))
+        elif keyword_id == "update_keyword":
             # update files database
-            self.update(extension)
-            return DoNothingAction()
+            cmd = ' '.join(['updatedb', '--require-visibility', '0', '--output', database_filepath])
+            logger.debug('Database update command: %s ' % cmd)
+            return RunScriptAction(cmd)
 
-    def update(self, extension):
-        update_database(extension)
-
-    def generate_results(self, extension, pattern, locate_flags):
+    def generate_results(self, extension, pattern, locate_flags, open_script):
         for (f) in get_file_list(extension, pattern, locate_flags):
             path = '%s' % (f)
-            yield ExtensionSmallResultItem(icon=exec_icon,
-                                           name=path,
-                                           on_enter=OpenAction(path))
+            script = open_script + ' ' + path
+            yield ExtensionSmallResultItem(icon=exec_icon, name=path, on_enter=RunScriptAction(script))
 
 
 class ItemEnterEventListener(EventListener):
 
     def on_event(self, event, extension):
-        data = event.get_data()
-        self.open(extension, data['path'])
-
-    def open(self, extension, path):
-        cmd = ['open', path]
-        logger.info(' '.join(cmd))
-
-        try:
-            check_call(cmd) == 0
-            extension.show_notification("Done", "File %s open" % path, icon=dead_icon)
-        except CalledProcessError as e:
-            extension.show_notification("Error", "code %s" % e.returncode)
-        except Exception as e:
-            logger.error('%s: %s' % (type(e).__name__, e))
-            extension.show_notification("Error", "Check the logs")
-            raise
+        logger.debug('Ignored')
 
 
 def get_file_list(extension, pattern, flags):
@@ -116,24 +94,6 @@ def get_file_list(extension, pattern, flags):
     for line in out.split('\n'):
         f = line
         yield (f)
-
-def update_database(extension):
-    """
-    Update the database.
-    """
-    cmd = ['updatedb', '--require-visibility', '0', '--output', database_filepath]
-
-    logger.debug('Updating database: %s ' % (','.join(cmd)))
-
-    try:
-        check_call(cmd) == 0
-        extension.show_notification("Done", "It's updated now", icon=dead_icon)
-    except CalledProcessError as e:
-        extension.show_notification("Error", "'updatedb' returned code %s" % e.returncode)
-    except Exception as e:
-        logger.error('%s: %s' % (type(e).__name__, e))
-        extension.show_notification("Error", "Check the logs")
-        raise
 
 if __name__ == '__main__':
     LocateExtension().run()
